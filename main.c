d585c051ce68bdc2135291eb0121b959eaf9da8d
c-flory <29979806+c-flory@users.noreply.github.com>
2018-10-15 13:23:21 +0100

    Basic skeleton of functions

---
+++ main.c
@@ -0,0 +1,28 @@
+// Catherine Flory && Danni Popova
+// Real Time Embedded Systems
+
+#include <cinterface.h>
+#include <stdio.h>
+#include <vxWorks.h>
+#include <taskLib.h>
+#include <stdlib.h>
+
+void Sensor1(void){
+
+}
+
+void Sensor2(void){
+
+}
+
+void MotorController(void){
+
+}
+
+void Feedback(LargeBlockCount, SmallBlockCount){
+
+}
+
+void Main(void){
+
+}
