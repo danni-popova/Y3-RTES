e8a8810c28ff9b87736898e583b8e6fe84075442
Catherine Flory <UP781262@uni.ds.port.ac.uk>
2018-10-16 13:43:33 +0100

    Adding State 6 byte array to pass info between functions

--- main.c
+++ main.c
@@ -23,25 +23,26 @@ void Settings(void){
   // Initial settings that can be altered by the user
 }
 
-void CheckSensor(char CountConveyor1, char ObjectSizeConveyor1){
+void CheckSensor(char State){
   // Check sensors and Count operation
-  // Sensors will cycle through state 0, 1, 2 if small block
-  // Sensors will cycle through state 0, 1, 3, 2 if large block
+  // Sensors will cycle through State 0, 1, 2 if small block
+  // Sensors will cycle through State 0, 1, 3, 2 if large block
 
   // readSizeSensors returns 0, 1, 2, 3 for no object, sensor 1, sensor 2, and
   // sensor 1 & 2, respectively.
   // The conveyor parameter distinguishes which conveyor is being checked.
   // Input is either 0 or 1.
-  char conveyor = 0;
-  char stateconveyor0 = readSizeSensors(char conveyor);
-  ~ conveyor;
-  char stateconveyor1 = readSizeSensors(char conveyor);
+  char stateconveyor0 = readSizeSensors(0);
+  char stateconveyor1 = readSizeSensors(1);
+  // Put data into an array and compare with previous State. Flip State change
+  // to 0 if identical. Save previous State.
+  strncpy(char LastState[], State, 6);
   // return array of 6 characters
 }
 
 void MotorController(void){
   // Gate controller operation controls both gates
-  setGates(char state);
+  setGates(char State);
 }
 
 void Feedback(LargeBlockDetectConveyor1, SmallBlockDetectConveyor1,
@@ -53,18 +54,22 @@ void Feedback(LargeBlockDetectConveyor1, SmallBlockDetectConveyor1,
 }
 
 void Main(void){
-  // char state buffer holds conveyor info.
-  // state change | conveyor | state conveyor 1| state conveyor 2 |
+  // char State buffer holds conveyor info.
+  // State change | conveyor | State conveyor 1| State conveyor 2 |
   // count conveyor 1 | count conveyor 2
-  char state[6] = {};
+  // 0 is null or no State change
+  // Initialise State to 0
+  unsigned char State[6] = {0, 0, 0, 0, 0, 0};
   startMotor(); // startMotor function from cinterface.h
   while(1){
     Settings();
-    state = CheckSensor();
-    // If there is a state change then Motor is used.
-    if (state){
+    // Update current State
+    State = CheckSensor(State);
+    // If there is a State change then Motor is used.
+    if (State[0] == 1){
       MotorController();
     }
+    // Should Feedback be in conditional statement or not?
     Feedback();
   }
 }
