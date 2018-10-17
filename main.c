c61eebe475f0c2308e392c9980e7eb1eeda2a081
c-flory <29979806+c-flory@users.noreply.github.com>
2018-10-17 10:12:10 +0100

    Managing array buffers for memory of previous state

--- main.c
+++ main.c
@@ -23,19 +23,35 @@ void Settings(void){
   // Initial settings that can be altered by the user
 }
 
-void CheckSensor(char State, char LastState){
+char CompareBuffers(char State[], char LastState[]){
+  for(char i=1; i<5; i++) {     // Iterate through State buffers from 1 to 5
+    if(State[i] != LasteState[i]) {
+      return 1;
+    }
+     // State has not changed
+     return 0;
+  }
+}
+
+void CheckSensor(char State[], char LastState[]){
   // Check sensors and Count operation
   // Sensors will cycle through State 0, 1, 2 if small block
   // Sensors will cycle through State 0, 1, 3, 2 if large block
 
+  // If reset is true, reset buffers
+  if State[5] == 1{
+    State[] = {0, 0, 0, 0, 0, 0};
+    unsigned char LastState[] = {0, 0, 0, 0, 0, 0, 0};
+  }
   // readSizeSensors returns 0, 1, 2, 3 for no object, sensor 1, sensor 2, and
   // sensor 1 & 2, respectively.
   // The conveyor parameter distinguishes which conveyor is being checked.
   // Input is either 0 or 1.
-  char stateconveyor0 = readSizeSensors(0);
-  char stateconveyor1 = readSizeSensors(1);
+  char State[1] = readSizeSensors(0);
+  char State[2] = readSizeSensors(1);
   // Put data into an array and compare with previous State. Flip State change
   // to 0 if identical.
+  State[0] = CompareBuffers(char State[], char LastState[])
   // Save previous State.
   strncpy(char LastState, State, 6);
   // return array of 6 characters
@@ -56,17 +72,17 @@ void Feedback(LargeBlockDetectConveyor1, SmallBlockDetectConveyor1,
 
 void Main(void){
   // char State buffer holds conveyor info.
-  // State change | conveyor | State conveyor 1| State conveyor 2 |
-  // count conveyor 1 | count conveyor 2
+  // State change | State conveyor 1 | State conveyor 2 |
+  // count conveyor 1 | count conveyor 2 | initialise or reset
   // 0 is null or no State change
-  // Initialise State to 0
-  unsigned char State[6] = {0, 0, 0, 0, 0, 0};
-  unsigned char LastState[6] = {0, 0, 0, 0, 0, 0};
+  // Initialise State buffer to 0
+  unsigned char State[] = {0, 0, 0, 0, 0, 1};
+  unsigned char LastState[] = {0, 0, 0, 0, 0, 0};
   startMotor(); // startMotor function from cinterface.h
   while(1){
     Settings();
     // Update current State
-    State = CheckSensor(State, LastState);
+    State = CheckSensor(State[], LastState[]);
     // If there is a State change then Motor is used.
     if (State[0] == 1){
       MotorController();
