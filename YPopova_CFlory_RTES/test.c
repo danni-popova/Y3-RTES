2bbb7fbed57d4859755d4c458fdc58af677df846
Yordanka Popova <43949046+danni-popova@users.noreply.github.com>
2018-11-12 11:01:15 +0000

    Add count big and small blocks

--- YPopova_CFlory_RTES/test.c
+++ YPopova_CFlory_RTES/test.c
@@ -4,94 +4,107 @@
 #include "stdio.h"
 #include "cinterface.h"
 
-int mainBeltPattern[] = {0, 0, 0};
-int secondaryBeltPattern[] = {0, 0, 0};
-
 int blocksDetected = 0;
-int blocksCounted = 0;
 
-int smallBlocksCounter = 0;
-int bigBlocksCounter = 0;
-char lastSensorResult = 0;
+int smallBlocksDetected = 0;
+int bigBlocksDetected = 0;
 
-void main(void)
+
+void PrintStatus(void) 
 {
-    startMotor();
-
-    while(1)
-    {
-        
+    while (1) {
+    	printf("Total blocks detected: %d \n", blocksDetected);
+    	printf("Small blocks detected: %d \n", smallBlocksDetected);
+    	
+    	bigBlocksDetected = blocksDetected - smallBlocksDetected; 
+    	
+    	printf("Big blocks detected: %d \n", bigBlocksDetected);
+    	
+        taskDelay(1 * sysClkRateGet());
     }
 }
 
-void readMainSensors(void)
+void readMainSensors(void) 
 {
-      // Reset has to be called before read
-      resetSizeSensors(0);
-
-      char result = readSizeSensors(0);
-
-      if(result != lastSensorResult)
-      {
-          switch (result)
-          {
-            case 0:
-              printf("No object currently in front of size sensors");
-            case 1:
-              printf("An object in front of first sensor");
-              blocksDetected++;
-            case 2:
-              printf("An object in front of the second sensor");
-            case 3:
-              printf("Object(s) in front of both sensors");
-          }
-          // detect pattern change for small and big blocks
-          if(lastSensorResult[0] == 1 && lastSensorResult[1]==0)
-          {
-              if(result == 0)
-              {
-                printf("Small block on belt 0");
-                smallBlocksCounter++;
-              }
-              else if (result == 3)
-              {
-                printf("Big block on belt 0");
-                bigBlocksCounter++;
-              }
-          }
-      }
+	char newMainState = 0; 
+	char oldMainState = 0;
+	
+	while(1)
+	{
+		resetSizeSensors(0);
+		
+		newMainState = readSizeSensors(0);
+		
+		if(newMainState != oldMainState)
+		{
+			switch(newMainState)
+			{
+				case 0 : 
+					printf("No blocks in front of size sensors\n");
+					
+					if(oldMainState == 1)
+						smallBlocksDetected++;
+					
+					break;
+				case 1:
+					printf("Block in front of first size sensor\n");
+					blocksDetected++;
+					break;
+				case 2:
+					printf("Block in front of second size sensor\n");
+					break;
+				case 3:
+					printf("Block(s) in front of both size sensors\n");
+					break;
+			}
+			
+			oldMainState = newMainState; 
+		}
+	}
 }
 
-void readSecondarySensors(void)
-{
-    // Reset has to be called before read
-    resetSizeSensors(1);
+void main(void) 
+{		
+	startMotor();
 
-    char result = readSizeSensors(1);
+	int readMainSensorsId;	
+	readMainSensorsId = taskSpawn("ReadingMainSensors", 100, 0, 20000, (FUNCPTR)readMainSensors, 0,0,0,0,0,0,0,0,0,0);
+
+	int printStatusId;
+	printStatusId = taskSpawn("PrintingBlocksCount", 99, 0, 20000, (FUNCPTR)PrintStatus, 0,0,0,0,0,0,0,0,0,0); 
+	
+	taskDelay(10 * sysClkRateGet());
+
+	taskDelete(readMainSensorsId);
+	taskDelete(printStatusId);
+
+	stopMotor();
+
+	exit(0);
 }
 
-void controlGates(char command)
+void stop(void)
 {
-    switch(command)
-    {
-      case 0 :
-        printf("Opening both gates.");
-        break;
-      case 1 :
-        printf("Open gate 0, close gate 1");
-        break;
-      case 2:
-        printf("Close gate 0, open gate 1");
-        break;
-      case 3:
-        printf("Closing both gates.");
-        break;
-    }
-
-    setGates(command);
+	stopMotor();
 }
 
-void callStopMotor(void)
+void controlGates(char command) 
 {
-    stopMotor();
+	switch (command) 
+	{
+		case 0:
+			printf("Opening both gates.");
+			break;
+		case 1:
+			printf("Open gate 0, close gate 1");
+			break;
+		case 2:
+			printf("Close gate 0, open gate 1");
+			break;
+		case 3:
+			printf("Closing both gates.");
+			break;
+	}
+
+	setGates(command);
 }
